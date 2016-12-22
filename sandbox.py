dae080150e1cf45113887ab0fda374bf8f360695
heinz rellup <heinz.rellup@freenet.de>
2016-12-22 12:35:47 +0100

    code

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,10 @@
 #test
+import textfsm
+
+template = open('examples/show_chassis_routing-engine_template.txt','r')
+
+with open('examples/show_chassis_routing-engine_output.txt', 'r') as myfile:
+    data=myfile.read().replace('\n', '')
+
+re_table = textfsm.TextFSM(template)
+data = re_table.ParseText(data)
