a09450b5f54c7aa94c6077aa98144866aa6efe12
heinz rellup <heinz.rellup@freenet.de>
2016-12-22 15:20:05 +0100

    newcode

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,22 @@
 #test
 import textfsm
 
-template = open('examples/show_chassis_routing-engine_template.txt','r')
 
-with open('examples/show_chassis_routing-engine_example.txt', 'r') as myfile:
-    data=myfile.read().replace('\n', '')
 
+template = open('examples/show_clock_temp.txt','r')
+
+with open('examples/show_clock_out.txt', 'r') as myfile:
+    output=myfile.read().replace('\n', '')
+
+
+print output
+print template
 re_table = textfsm.TextFSM(template)
-data = re_table.ParseText(data)
+data = re_table.ParseText(output)
+
+
+print ', '.join(re_table.header)
+
+print data
+for row in data:
+ print ', '.join(row)
